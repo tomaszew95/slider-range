de3cda2fa6c4cf2eb9adba019dbd7ce6587f817f
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-05-06 12:35:53 +0200

    console.log()

--- slider_range.js
+++ slider_range.js
@@ -135,7 +135,9 @@ var controlSlider = (e, sliderInfo) =>{
         }
         //if not defined it is equal 6px
         // controllerContainer.style.width = sliderInfo.lineSize + 'px';
-        controllerContainer.style.width = window.getComputedStyle(document.documentElement).setProperty('--controller-container-thickness', (sliderInfo.lineSize + 'px'));
+        // controllerContainer.style.width = window.getComputedStyle(document.documentElement).setProperty('--controller-container-thickness', (sliderInfo.lineSize + 'px'));
+        console.log(controllerContainer.style.width);
+        console.log(window.getComputedStyle(document.documentElement).setProperty('--controller-container-thickness', (sliderInfo.lineSize + 'px')));
         if(sliderInfo.hideCta=='true'){
             controller.style.opacity = 0;
         }
