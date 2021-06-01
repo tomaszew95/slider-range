7f9aa86e79562a2e9553796b38b155a01160928c
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-06-01 14:00:47 +0200

    test_01

--- slider_range.js
+++ slider_range.js
@@ -151,8 +151,8 @@ var controlSlider = (e, sliderInfo) =>{
         }
         controller.style.transform = 'scale(' + sliderInfo.ctaScale + ')';
         if(sliderInfo.setMargin=='true'){
-            // margins = ((($(controller).width()*sliderInfo.ctaScale)/2)*100)/($(sliderContainer).width());
-            margins = ((((parseFloat(controller.style.width))*sliderInfo.ctaScale)/2)*100)/(parseFloat(sliderContainer.style.width));
+            margins = ((($(controller).width()*parseFloat(sliderInfo.ctaScale))/2)*100)/($(sliderContainer).width());
+            // margins = ((((parseFloat(controller.style.width))*sliderInfo.ctaScale)/2)*100)/(parseFloat(sliderContainer.style.width));
             console.log(margins, $(controller).width(), sliderInfo.ctaScale, $(sliderContainer).width());
         }
     }
